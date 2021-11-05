1fe550e83b6490d9b8dd252ae485b55e43546605
qiwei <qiweiyang@berkeley.edu>
2021-11-05 13:25:04 -0700

    add config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_go7otKIsI',
+        userPoolClientId: 'o3fqacqfu30coo68un744llal',
+        region: 'us-west-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
