e91f931471671246789092f9a062400a5f0ad9f8
qiwei <qiweiyang@berkeley.edu>
2021-11-05 22:07:26 -0700

    config fiels

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_go7otKIsI',
-        userPoolClientId: 'o3fqacqfu30coo68un744llal',
-        region: 'us-west-2'
+        userPoolId: 'us-west-2_ANjkafIB1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '754nf1dvfun7kbmjf60j3tgmvp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
